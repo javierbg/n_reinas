702cf88a9d91a2b20d979e03f259d356b588a969
javierbg <i32bagoj@uco.es>
2015-05-13 00:33:06 +0200

    Añadida búsqueda en amplitud

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Distintos acercamientos al problema de las n-reinas, con el propósito de aprend
 
 Métodos implementados:
 * Hill Climbing (escalada): Buscar siempre un hijo que mejore el coste inicial, si no se encuentra se empieza en un nuevo punto aleatorio
+* Amplitud: Mantiene un conjunto de estados explorados y frontera. Toma el de menor coste de frontera y genera sus hijos. Si éstos no estan explorados los añade a frontera. Repite hasta que el estado que toma es de coste 0.
---
+++ amplitud/busqueda.py
@@ -0,0 +1,33 @@
+from queens import *
+
+dim = 3
+
+
+t = Tablero(dim)
+inicializaTableroAleatorio(t)
+frontera = [t]
+explorados = []
+mejorCoste = t.coste()
+
+while frontera: #Mientras exista algún elemento en frontera
+	#Extrae el estado de menor coste de frontera
+	actual = frontera.pop(frontera.index(min(frontera)))
+	
+	if actual.coste() == 0: #Esto significa que lo ha encontrado
+		break
+	
+	#Lo añade a explorados
+	explorados.append(actual)
+	
+	hijos = generaHijosTablero(actual)
+	
+	for h in hijos:
+		if h not in explorados:
+			frontera.append(h)
+
+if actual.coste() == 0:
+	print("Solución encontrada!")
+	actual.imprimir()
+	
+else:
+	print("No hay solución")
